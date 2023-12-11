62e3c002c70146baf716304e69f1895036a026b6
Sean Malloy <techfan321@gmail.com>
2023-12-11 08:59:17 -0500

    Day 11 cleanup

--- solutions/d11.py
+++ solutions/d11.py
@@ -6,21 +6,33 @@ from dataclasses import dataclass
 
 @dataclass
 class CosmicData:
-    grid: list[str]
     galaxies: list[Point]
+    empty_rows: list[int]
+    empty_cols: list[int]
 
 
 @advent.parser(11)
 def parse(file: TextIOWrapper):
     grid = [list(map(str, line.strip())) for line in file.readlines()]
+
     galaxies = []
     for r, row in enumerate(grid):
         for c, col in enumerate(row):
             if col == '#':
                 galaxies.append(Point(r, c))
-    return CosmicData(grid, galaxies)
+    
+    empty_rows = []
+    for r, row in enumerate(grid):
+        if row.count('#') == 0:
+            empty_rows.append(r)
+    
+    empty_cols = []
+    for c, col in enumerate(zip(*grid)):
+        if col.count('#') == 0:
+            empty_cols.append(c)
 
-INF = 2**31-1
+    return CosmicData(galaxies, empty_rows, empty_cols)
+
 
 @advent.day(11, part=1)
 def solve1(data: CosmicData):
@@ -43,29 +55,18 @@ def total_dists(galaxies: list[Point]):
 
 def expand(data: CosmicData, N: int):
     N -= 1
-
-    row_expands = []
-    for r, row in enumerate(data.grid):
-        if row.count('.') == len(data.grid[r]):
-            row_expands.append(r)
-    
-    col_expands = []
-    for c, line in enumerate(zip(*data.grid)):
-        if line.count('.') == len(data.grid):
-            col_expands.append(c)
-
     row_deltas = [0]*len(data.galaxies)
-    for r in reversed(row_expands):
+    for r in reversed(data.empty_rows):
         for g, galaxy in enumerate(data.galaxies):
             if galaxy.r > r:
                 row_deltas[g] += N
     
     col_deltas = [0]*len(data.galaxies)
-    for c in reversed(col_expands):
+    for c in reversed(data.empty_cols):
         for g, galaxy in enumerate(data.galaxies):
             if galaxy.c > c:
                 col_deltas[g] += N
-    
+
     expanded = []
     for g,dr,dc in zip(data.galaxies, row_deltas, col_deltas):
         expanded.append(Point(g.r + dr, g.c + dc))
