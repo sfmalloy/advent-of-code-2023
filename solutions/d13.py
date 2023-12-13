ea05fd4fb1c453fe5dddb40e7802ca9e04e9d248
Sean Malloy <techfan321@gmail.com>
2023-12-13 17:10:12 -0500

    Day 13

---
+++ solutions/d13.py
@@ -0,0 +1,51 @@
+from .lib.advent import advent
+from io import TextIOWrapper
+from typing import Sequence
+
+
+@advent.parser(13)
+def parse(file: TextIOWrapper):
+    return [line.strip().splitlines() for line in file.read().split('\n\n')]
+
+
+@advent.day(13, part=1)
+def solve1(patterns: list[list[str]]):
+    return find_reflection(patterns, 0)
+
+
+@advent.day(13, part=2)
+def solve2(patterns: list[list[str]]):
+    return find_reflection(patterns, 1)
+
+
+def find_reflection(patterns: list[list[str]], smudge_count: int):
+    total = 0
+    for pattern in patterns:
+        horizontal_line = search_pattern(pattern, smudge_count)
+        if horizontal_line == 0:
+            pattern = [''.join(row) for row in zip(*pattern)]
+            vertical_line = search_pattern(pattern, smudge_count)
+            total += vertical_line
+        else:
+            total += 100 * horizontal_line
+    return total
+
+
+def search_pattern(pattern: list[list[str]], smudge_count: int):
+    for row in range(0, len(pattern) - 1):
+        if count_smudges(pattern, row, row + 1) == smudge_count:
+            return row + 1
+    return 0
+
+
+def count_smudges(pattern: list[str], start: int, end: int):
+    left = start
+    right = end
+    smudges = 0
+    while left >= 0 and right < len(pattern):
+        if pattern[left] != pattern[right]:
+            for l, r in zip(pattern[left], pattern[right]):
+                smudges += l != r
+        left -= 1
+        right += 1
+    return smudges
