e9c2efcfe7c4ccb6e69db066df14c6ec45de6983
Sean Malloy <techfan321@gmail.com>
2023-12-17 14:30:13 -0500

    Day 17 cleanup

--- solutions/d17.py
+++ solutions/d17.py
@@ -2,7 +2,7 @@ from .lib.advent import advent
 from .lib.util import Point, Dir
 from io import TextIOWrapper
 from collections import defaultdict
-from dataclasses import dataclass, field
+from dataclasses import dataclass
 from queue import PriorityQueue
 
 INF = 2**31-1
@@ -23,44 +23,28 @@ class Block:
     cost: int
     pos: Point
 
-    def __repr__(self):
-        return f'{self.cost}'
-
 
 @advent.parser(17)
 def parse(file: TextIOWrapper) -> list[list[Block]]:
-    return [[Block(int(digit), Point(row, col)) for col, digit in enumerate(line.strip())] for row, line in enumerate(file.readlines())]
+    return [[Block(int(digit), Point(row, col)) for col, digit in enumerate(line.strip())] 
+            for row, line in enumerate(file.readlines())]
 
 
 @advent.day(17, part=1)
 def solve1(grid: list[list[Block]]):
-    start = Crucible(Point(0, 0), Dir.E, 0)
-    q: PriorityQueue[Crucible] = PriorityQueue()
-    q.put(start)
-    
-    dists = defaultdict(lambda: INF)
-    goal = grid[-1][-1].pos
+    return follow_path(grid, 3)
 
-    while not q.empty():
-        crucible = q.get()
-        if crucible.pos == goal:
-            return crucible.heat_loss
-        for dir in Dir.all() - {Dir.opposite(crucible.dir)}:
-            if (dir == crucible.dir and crucible.dir_dist < 3) or dir != crucible.dir:
-                pos = crucible.pos + dir
-                if pos.in_bounds(grid):
-                    dir_dist = (crucible.dir_dist + 1) if crucible.dir == dir else 1
-                    heat_loss = crucible.heat_loss + grid[pos.r][pos.c].cost
-                    if heat_loss < dists[(pos, dir, dir_dist)]:
-                        dists[(pos, dir, dir_dist)] = heat_loss
-                        q.put(Crucible(pos, dir, heat_loss, dir_dist))
 
 @advent.day(17, part=2)
 def solve2(grid: list[list[Block]]):
+    return follow_path(grid, 10, 4)
+
+
+def follow_path(grid: list[list[Block]], max_dir_dist: int, min_dir_dist: int=1):
     start = Crucible(Point(0, 0), Dir.E, 0)
     q: PriorityQueue[Crucible] = PriorityQueue()
     q.put(start)
-    
+
     dists = defaultdict(lambda: INF)
     goal = grid[-1][-1].pos
 
@@ -69,7 +53,7 @@ def solve2(grid: list[list[Block]]):
         if crucible.pos == goal:
             return crucible.heat_loss
         for dir in Dir.all() - {Dir.opposite(crucible.dir)}:
-            if (dir == crucible.dir and crucible.dir_dist < 10) or (dir != crucible.dir and crucible.dir_dist >= 4):
+            if (dir == crucible.dir and crucible.dir_dist < max_dir_dist) or (dir != crucible.dir and crucible.dir_dist >= min_dir_dist):
                 pos = crucible.pos + dir
                 if pos.in_bounds(grid):
                     dir_dist = (crucible.dir_dist + 1) if crucible.dir == dir else 1
@@ -77,26 +61,3 @@ def solve2(grid: list[list[Block]]):
                     if heat_loss < dists[(pos, dir, dir_dist)]:
                         dists[(pos, dir, dir_dist)] = heat_loss
                         q.put(Crucible(pos, dir, heat_loss, dir_dist))
-
-
-def print_path(grid: list[list[Block]], path: list[Point]):
-    symbols = {
-        Dir.N: '^',
-        Dir.S: 'v',
-        Dir.E: '>',
-        Dir.W: '<'
-    }
-    prev = Point(0, 0)
-    # printed = [[block.cost for block in row] for row in grid]
-    total = 0
-    printed = [['.' for _ in row] for row in grid]
-    for p in path:
-        printed[p.r][p.c] = symbols[p - prev]
-        prev = p
-        total += grid[p.r][p.c].cost
-    for r in printed:
-        for c in r:
-            print(c, end='')
-        print()
-    print(f'Total = {total}')
-# 1110 too high
