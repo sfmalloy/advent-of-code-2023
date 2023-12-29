b54086032567831f334d7a11d9d585f9af261a4b
Sean Malloy <techfan321@gmail.com>
2023-12-29 09:08:38 -0500

    Day 21 part 2

--- solutions/d21.py
+++ solutions/d21.py
@@ -2,7 +2,7 @@ from .lib.advent import advent
 from .lib.util import Point, PointDir
 from io import TextIOWrapper
 from dataclasses import dataclass
-from collections import deque, defaultdict
+from collections import deque
 
 
 @dataclass
@@ -29,12 +29,41 @@ def parse(file: TextIOWrapper):
 
 @advent.day(21, part=1)
 def solve1(ipt: Garden):
+    return len(exact_dist_points(ipt, 64))
+
+
+@advent.day(21, part=2)
+def solve2(ipt: Garden):
+    S = 26501365
+    Y = [len(exact_dist_points(ipt, 65)),
+         len(exact_dist_points(ipt, 196)),
+         len(exact_dist_points(ipt, 327))]
+    X = [65, 196, 327]
+    A = len(ipt.garden) * len(ipt.garden)
+
+    v0 = lagrange(X[2], X[1], X[0], Y[0])
+    v1 = lagrange(X[2], X[0], X[1], Y[1])
+    v2 = lagrange(X[0], X[1], X[2], Y[2])
+
+    a = round((v0[0] + v1[0] + v2[0]) * A)
+    b = round((v0[1] + v1[1] + v2[1]) * A)
+    c = round((v0[2] + v1[2] + v2[2]) * A)
+
+    return (a*(S**2) + b*S + c) // A
+
+
+def lagrange(a, b, c, y):
+    D = (c-a) * (c-b)
+    return (y / D, -y*(a+b) / D, y*(a*b) / D)
+
+
+def exact_dist_points(ipt: Garden, S: int):
+    L = len(ipt.garden)
     q = deque([(ipt.start, 0)])
     seen = set()
     final = set()
-    S = 64
     is_even = S % 2
-    while len(q) > 0:
+    while q:
         pos, steps = q.popleft()
         if pos in seen:
             continue
@@ -46,85 +75,6 @@ def solve1(ipt: Garden):
         seen.add(pos)
         for d in PointDir.all():
             new = pos + d
-            if new.in_bounds(ipt.garden) and ipt.garden[new.r][new.c] == '.':
+            if ipt.garden[new.r % L][new.c % L] == '.':
                 q.append((new, steps + 1))
-    return len(final)
-
-
-@advent.day(21, part=2)
-def solve2(ipt: Garden):
-    return 'not solved'
-
-
-def print_ppm(grid: list[list[str]], counts: defaultdict[set]):
-    import sys
-    import colorsys
-
-    def write(*args):
-        print(*args, file=sys.stderr)
-
-    mn = min(len(count) for count in counts.values())
-    mx = max(len(count) for count in counts.values()) + 1
-    print(mn, mx)
-
-    write('P3')
-    write(len(grid), len(grid[0]))
-    write('255')
-    for r in range(len(grid)):
-        for c in range(len(grid[0])):
-            if Point(r, c) in counts:
-                percent = (mx - len(counts[Point(r, c)])) / (mx - mn)
-                write(' '.join(list(map(str, tuple(round(e*255) for e in colorsys.hsv_to_rgb(1, 0.5, percent))))))
-            else:
-                write('255 255 255')
-
-
-    # q = deque([(ipt.start, 0)])
-    # seen = set()
-    # final = set()
-    # stepdict = defaultdict(list)
-    # # counts = defaultdict(lambda: defaultdict(set))
-    # counts = defaultdict(set)
-    # # S = 26_501_365
-    # S = int(input())
-    # is_even = S % 2
-    # while len(q) > 0:
-    #     pos, steps = q.popleft()
-    #     if pos in seen:
-    #         continue
-    #     if steps == S:
-    #         stepdict[Point(pos.r % len(ipt.garden), pos.c % len(ipt.garden[0]))].append(steps)
-    #         counts[(Point(pos.r % len(ipt.garden), pos.c % len(ipt.garden[0])))].add(pos)
-    #         # counts[pos.r % len(ipt.garden)][pos.c % len(ipt.garden[0])].add((pos, dir))
-    #         continue
-    #     if steps % 2 == is_even:
-    #         # counts[pos.r % len(ipt.garden)][pos.c % len(ipt.garden[0])].add((pos, dir))
-    #         counts[(Point(pos.r % len(ipt.garden), pos.c % len(ipt.garden[0])))].add(pos)
-    #         stepdict[Point(pos.r % len(ipt.garden), pos.c % len(ipt.garden[0]))].append(steps)
-    #     seen.add(pos)
-    #     for d in PointDir.all():
-    #         new = pos + d
-    #         if ipt.garden[new.r % len(ipt.garden)][new.c % len(ipt.garden[0])] == '.':
-    #             q.append((new, steps + 1))
-    # tot = 0
-    # unique = defaultdict(list)
-    # count_counts = defaultdict(int)
-    # for r in range(len(ipt.garden)):
-    #     for c in range(len(ipt.garden[0])):
-    #         unique[tuple(stepdict[Point(r, c)])].append((r,c))
-
-    # for u in unique.items():
-    #     print(u)
-    # # print_ppm(ipt.garden, counts)
-    # # for k, v in counts.items():
-    # #     print(k, len(v), file=sys.stderr)
-    # #     tot += len(v)
-    # # for r in range(len(ipt.garden)):
-    # #     for c in range(len(ipt.garden[0])):
-    # #         if ipt.garden[r][c] != '#':
-    # #             # print(r, c, len(counts[r][c]))
-    # #             tot += len(counts[r][c])
-    # #             count_counts[len(counts[r][c])] += 1
-    
-    # # for k, v in sorted(count_counts.items()):
-    # #     print(k, v)
+    return final
